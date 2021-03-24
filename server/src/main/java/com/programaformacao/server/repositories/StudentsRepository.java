0030872e152c6dac0ec5c10b07b60b13ffe5da81
ruizseh <fsergioruiz@hotmail.com>
2021-03-24 19:02:17 -0300

    Fix

---
+++ server/src/main/java/com/programaformacao/server/repositories/StudentsRepository.java
@@ -0,0 +1,9 @@
+package com.programaformacao.server.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.programaformacao.server.models.Students;
+
+public interface StudentsRepository extends JpaRepository<Students, Long> {
+
+}
