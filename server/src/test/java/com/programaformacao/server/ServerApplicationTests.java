b469489431b8ef1c8dca362019f0e2ba04b0e215
brunofow <brunocamposfow@gmail.com>
2021-03-25 13:02:08 -0300

    Fix test errors

--- server/src/test/java/com/programaformacao/server/ServerApplicationTests.java
+++ server/src/test/java/com/programaformacao/server/ServerApplicationTests.java
@@ -1,12 +1,9 @@
 package com.programaformacao.server;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class ServerApplicationTests {
 
-	@Test
+
 	void contextLoads() {
 	}
 
