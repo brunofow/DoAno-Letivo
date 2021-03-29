0295953e89528e95aa5989f5a6a9293a1d28bf0f
brunofow <brunocamposfow@gmail.com>
2021-03-29 10:45:50 -0300

    CORS Config

--- server/src/main/java/com/programaformacao/server/config/WebConfig.java
+++ server/src/main/java/com/programaformacao/server/config/WebConfig.java
@@ -1,2 +1,17 @@
-package com.programaformacao.server.config;public class WebConfig {
+package com.programaformacao.server.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+
+@Configuration
+@EnableWebMvc
+public class WebConfig extends WebMvcConfigurerAdapter {
+
+  @Override
+  public void addCorsMappings(CorsRegistry registry) {
+    registry.addMapping("/**");
+  }
 }
