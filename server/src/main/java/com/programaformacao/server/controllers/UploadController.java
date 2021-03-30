25c1acabb1bef0e69baff21955789ed73b778caf
brunofow <brunocamposfow@gmail.com>
2021-03-30 15:59:52 -0300

    Get files uploaded

--- server/src/main/java/com/programaformacao/server/controllers/UploadController.java
+++ server/src/main/java/com/programaformacao/server/controllers/UploadController.java
@@ -1,16 +1,19 @@
 package com.programaformacao.server.controllers;
 
+import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Date;
 
+import org.apache.commons.io.IOUtils;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -18,15 +21,23 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 @RequestMapping("/upload")
 public class UploadController {
    
-   private static String UPLOADED_FOLDER = "C:\\Users\\NAJA INFORMATICA\\Desktop\\Nova";
-   
-   @GetMapping("/")
-   public String index() {
-       return "upload";
-   }
+   private static String UPLOADED_FOLDER = "src/main/uploads/";
+
+	@GetMapping(value = "/photo/{photo}")
+	@ResponseBody
+	public ResponseEntity<byte[]> getPhoto(@PathVariable String photo) throws IOException {
+
+		File imgPath = new File(UPLOADED_FOLDER + photo);
+
+		byte[] image = Files.readAllBytes(imgPath.toPath());
+		HttpHeaders headers = new HttpHeaders();
+		headers.setContentType(MediaType.IMAGE_JPEG);
+		headers.setContentLength(image.length);
+		return new ResponseEntity<>(image, headers, HttpStatus.OK);
+	}
    
    @PostMapping
-   public ResponseEntity<String> singleFileUpload(@RequestParam("file") MultipartFile file,
+   public ResponseEntity<?> singleFileUpload(@RequestParam("file") MultipartFile file,
            RedirectAttributes redirectAttributes) {
 
 	if (file.isEmpty()) {
@@ -35,19 +46,18 @@ public class UploadController {
 	}
 
 	try {
-	
+	Date date = new Date();
+	String url = date + file.getOriginalFilename();
 	byte[] bytes = file.getBytes();
-	Path path = Paths.get(UPLOADED_FOLDER + file.getOriginalFilename());
+	Path path = Paths.get(UPLOADED_FOLDER + date + file.getOriginalFilename());
 	Files.write(path, bytes);
-	
-	redirectAttributes.addFlashAttribute("message",
-	"You successfully uploaded '" + file.getOriginalFilename() + "'");
-	
+
+		return ResponseEntity.ok(url);
 	} catch (IOException e) {
 	e.printStackTrace();
+
+	return ResponseEntity.ok("Erro ao fazer upload da imagem");
 	}
-	
-	return ResponseEntity.ok("C:\\Users\\NAJA INFORMATICA\\Desktop\\Nova");
 	}
 	
    @GetMapping("/uploadStatus")
